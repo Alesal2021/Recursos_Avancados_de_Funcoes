c1127e71c5aaf85f6e8039238ff7b2d6c927ba2b
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-10 08:23:32 -0300

    Initial commit

---
+++ Try return else.py
@@ -0,0 +1,14 @@
+while True:
+    def div():
+        while True:
+            try:
+                s1 = int(input('Digite um numero:'))
+                s2 = int(input('Digite outro numero:'))
+                res = s1 / s2
+            except:
+                    print('Tem que digitar um valor inteiro')
+            else:
+                return res
+
+#Programa principal
+    print(div())
